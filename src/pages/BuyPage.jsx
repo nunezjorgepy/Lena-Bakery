4cbc39c99f285b5b724b8e6452dfc2e1c9c6abd5
Jorge Nuñez <nunez.jorge@hotmail.com>
2025-09-11 22:49:13 -0300

    La idea era crear la tarjeta de productos en la página de compra, pero terminé agregando el título de la página. Ahora a hacer las tarjetas y, como adicional, agregar h1 en todas las páginas y los meta-tag del head

--- src/pages/BuyPage.jsx
+++ src/pages/BuyPage.jsx
@@ -1,11 +1,18 @@
+import './BuyPage.css'
 import BuyProductCard from "../components/BuyProductCard"
 import HeaderComponent from "../components/HeaderComponent"
 
 
 function BuyPage() {
   return (
-    <div>
+    <div className="max-container">
         <HeaderComponent />
+        <main className="buy-main">
+          <h2 className="buy-h2">Nuestros Productos</h2>
+          <div className="cards-containers">
+            
+          </div>
+        </main>
     </div>
   )
 }
