9d41f6cbc3c7e5ffd45f1b19dca9a242a6a7a139
Jorge Nuñez <nunez.jorge@hotmail.com>
2025-09-14 13:08:05 -0300

    Al hacer click en el boton de Tu Pedido, la ventana baja hasta donde esta la lista a comprar. Cambié el position del boton a fixed (en vez de absolute), porque tiene que estar visible siempre. Falta que el botón desaparezca cuando hago click en él.

--- src/pages/BuyPage.jsx
+++ src/pages/BuyPage.jsx
@@ -3,21 +3,33 @@ import HeaderComponent from "../components/HeaderComponent";
 import AddedProduct from '../components/AddedProduct';
 
 import { Link } from 'react-router-dom';
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
 import { ProductContext } from '../context/product.context';
 
 
 function BuyPage() {
   const { HTMLProducts, HTMLAddedProducts, HTMLTotal, message } = useContext(ProductContext);
+  const yourOrder = useRef(null);
+
+  const goToOrder = () => {
+    /* Baja la página hasta donde esta tu pedido. */
+    const topDistance = yourOrder.current.getBoundingClientRect().top;
+    window.scrollTo({
+      top: topDistance,
+      behavior: 'smooth'
+    })
+  }
   
   return (
     <div className="max-container">
         <HeaderComponent />
         <main className="buy-main">
+          {/* Botón para ir al pedido */}
           <div className="get-to-order-continaer">
             <span className='get-to-span buy-h2'>Tu pedido</span>
-            <button className="get-to-btn"><i className="bi bi-arrow-down"></i></button>
+            <button className="get-to-btn"><i className="bi bi-arrow-down" onClick={() => goToOrder()}></i></button>
           </div>
+
           {/* Título */}
           <h2 className="buy-h2">Nuestros Productos</h2>
 
@@ -27,7 +39,7 @@ function BuyPage() {
           </ul>
 
           {/* Título */}
-          <h2 className="buy-h2">Tu Pedido</h2>
+          <h2 className="buy-h2" ref={yourOrder}>Tu Pedido</h2>
 
           {/* Lista de agregados */}
           <div className="items-added-container">
