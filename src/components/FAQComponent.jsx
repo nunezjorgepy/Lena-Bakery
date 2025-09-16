73d12f0c5fca31323cfeb7c43d3612471d104f5c
Jorge Nuñez <nunez.jorge@hotmail.com>
2025-09-15 21:58:50 -0300

    Funcionalidad de los botones para mostrar u ocultar la respuesta a la pregunta frecuente. Por alguna razón, no funciona la transición de la altura (height), pero eso es para otro momento. Falta asegurarme que sea responsive

--- src/components/FAQComponent.jsx
+++ src/components/FAQComponent.jsx
@@ -1,22 +1,28 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 function FAQComponent(props) {
     const faqBtn = useRef(null);
+    const faqAnswer = useRef(null);
+    const [btnText, setBtnText] = useState('+')
     const { question, answer } = props.faq;
 
     const changeBtnText = () => {
-        let faqSymbol = faqBtn.current;
-        faqSymbol.innerHTML = faqSymbol.innerHTML === '+' ? '-' : '+';
+        /* Muestra o esconde la respuesta según corresponda. CAmbia el símbolo mostrado en el botón entre + y -. */
+        btnText === '+' ? setBtnText('-') : setBtnText('+');
+        faqAnswer.current.classList.toggle('show-answer');
     }
+
     return (
         <>
             <div className="question-container">
             <div className="question">
                 <div className="faq-question">{question}</div>
-                <button className="faq-btn" ref={faqBtn} onClick={() => changeBtnText()}>+</button>
+                <button className="faq-btn" ref={faqBtn} onClick={() => changeBtnText()}>{btnText}</button>
             </div>
-            <div className="respuesta">
-                {answer}
+            <div ref={faqAnswer} className="respuesta">
+                <p className="answer-p">
+                    {answer}
+                </p>
             </div>
             </div>
         </>
--- src/constants/faqList.js
+++ src/constants/faqList.js
@@ -3,9 +3,9 @@ const faqList = [
         id: 1,
         question: "¿Cómo conservas tur productos?",
         answer: [
-            "Nuestros productos son naturales y sin conservantes.",
-            //"Por eso necesitan ciertos cuidados especiales",
-            //"Postres y cuchareables: Heladera (2 a 3 días)"
+            "Nuestros productos son naturales y sin conservantes. ",
+            "Por eso necesitan ciertos cuidados especiales. ",
+            "Postres y cuchareables: Heladera (2 a 3 días)"
         ],
         isShown: false,
     },
@@ -13,9 +13,9 @@ const faqList = [
         id: 2,
         question: "¿Cómo hacer un pedido?",
         answer: [
-            "Realiza tu pedido por Whatsapp con mínimo 48hs de anticipación.",
-            //"Entra a la sección 'Pedinos' para hacer tu órden",
-            //"Confirma tu pedido con una seña del 50%",
+            "Realiza tu pedido por Whatsapp con mínimo 48hs de anticipación. ",
+            "Entra a la sección 'Pedinos' para hacer tu órden. ",
+            "Confirma tu pedido con una seña del 50% ",
         ],
         isShown: false,
     },
@@ -23,9 +23,9 @@ const faqList = [
         id: 3,
         question: "Modos de envío",
         answer: [
-            "Retira en nuestro punto de entrega en Villa Raffo - Saenz Peña.",
-            //"Hacemos envíos a barrios alrededores sin costo o enviamos por Rappi.",
-            //"El costo del envío queda a cargo del cliente."
+            "Retira en nuestro punto de entrega en Villa Raffo - Saenz Peña. ",
+            "Hacemos envíos a barrios alrededores sin costo o enviamos por Rappi. ",
+            "El costo del envío queda a cargo del cliente."
         ],
         isShown: false,
     },
