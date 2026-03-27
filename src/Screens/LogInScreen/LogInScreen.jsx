7ebbb8b49d3c7d6aa96de57d795e7772b553d76b
Jorge Nuñez <nunez.jorge@hotmail.com>
2026-03-27 17:21:27 -0300

    Modifica el formComponent para que sea un flex en vez de un grid

--- src/Screens/LogInScreen/LogInScreen.jsx
+++ src/Screens/LogInScreen/LogInScreen.jsx
@@ -1,15 +1,12 @@
 import './LogInScreen.css'
 import HeaderComponent from '../../components/layout/HeaderComponent/HeaderComponent'
-import InformationFormComponent2 from '../../components/ui/InformationFormComponent2/InformationFormComponent2'
 
 function LogInScreen() {
     return (
         <>
             <HeaderComponent />
-            {/* <div>LogInScreen</div> */}
-            {/* TODO: borar el componente de InformationFormComponent2. Lo uso acá a modo de prueba */}
-            <main className='main-container'>
-                <InformationFormComponent2 />
+            <main>
+                <div>LogInScreen</div>
             </main>
         </>
     )
