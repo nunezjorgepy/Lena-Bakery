dfac7d98e5fd8eb83a1e6927beb80fb8c6b180ab
Jorge Nuñez <nunez.jorge@hotmail.com>
2026-03-29 14:52:30 -0300

    Añade las constantes para el formulario de login

---
+++ src/constants/logInForm.constants.js
@@ -0,0 +1,51 @@
+/* 
+    Constantes para el form de login
+*/
+
+export const LOG_IN_FORM_CONSTANTS = {
+    form_title: "Inicia sesión",
+    form_subtitle: "Ingresa tus datos para iniciar sesión",
+    sections: [
+        {
+            /* section_title: "Detalles de la cuenta",
+            section_number: "1", */
+            inputs: [
+                {
+                    id: "email",
+                    name: "email",
+                    label: "Email",
+                    placeholder: "Ingresa tu email",
+                    required: true,
+                    type: "text",
+                    flex: "flex-100",
+                    required: true
+                },
+                {
+                    id: "password",
+                    name: "password",
+                    label: "Contraseña",
+                    placeholder: "Ingresa tu contraseña",
+                    required: true,
+                    type: "password",
+                    flex: "flex-100",
+                    required: true
+                }
+            ]
+        }
+    ],
+    button: {
+        text: "Iniciar sesión",
+        type: "submit"
+    },
+    footer: {
+        text: "¿No tenés una cuenta?",
+        link_text: "Registrate",
+        link_to: "/register"
+    }
+}
+
+// initialFormState
+export const initialFormState = {
+    [LOG_IN_FORM_CONSTANTS.inputs[0].name]: "",
+    [LOG_IN_FORM_CONSTANTS.inputs[1].name]: ""
+}
