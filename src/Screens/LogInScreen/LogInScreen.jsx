9ab80d2b85b09ac249acdabfcdfca4e546590a3f
Jorge Nuñez <nunez.jorge@hotmail.com>
2026-03-29 20:08:22 -0300

    Al loguearse correctamente, redirecciona a la página Home

--- src/Screens/LogInScreen/LogInScreen.jsx
+++ src/Screens/LogInScreen/LogInScreen.jsx
@@ -4,11 +4,14 @@ import { LOG_IN_FORM_CONSTANTS, initialFormState } from '../../constants/logInFo
 import InformationFormComponent from '../../components/ui/InformationFormComponent/InformationFormComponent'
 import useRequest from '../../hooks/useRequest'
 import authService from '../../services/authService'
+import { useContext, useEffect } from 'react'
+import { AuthContext } from '../../context/authContext'
 
 function LogInScreen() {
     const { form_title, form_subtitle, sections, button, footer } = LOG_IN_FORM_CONSTANTS
 
     const { sendRequest, response, error, loading } = useRequest()
+    const { manageLogin } = useContext(AuthContext)
 
     const onLogIn = (formState) => {
         try {
@@ -21,6 +24,15 @@ function LogInScreen() {
             console.log(error)
         }
     }
+
+    useEffect(
+        () => {
+            if (response && response.status === 200) {
+                manageLogin(response.data.login_token)
+            }
+        }, 
+        [response]
+    )
 
     return (
         <>
