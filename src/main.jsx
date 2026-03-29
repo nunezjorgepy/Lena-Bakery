a1f26fd2464a3aabf4b0ccfba5aedb6d3b14b808
Jorge Nuñez <nunez.jorge@hotmail.com>
2026-03-29 19:47:16 -0300

    Agrega el contexto AuthContext al main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
+import AuthContextProvider from './context/authContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </BrowserRouter>
 )
